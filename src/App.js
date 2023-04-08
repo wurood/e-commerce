270be5b115fd2dd1af377ec8c5a563fcd378b097
unknown <wuroodj95@gmail.com>
2023-04-08 13:11:21 +0300

    update app.js folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, { Component }  from 'react';
 import './App.css';
 import Home from './components/Home/index';
-import Tittle from './components/Tittle/index'
+
+
 function App() {
   return (
     <>
-    <Tittle text="hello" />
+    <Home text="hello" />
     </>
   );
 }
