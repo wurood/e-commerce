56e7c7267d2d164d629e7203e5b2693893784119
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-04-21 00:20:23 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-
 import React from "react";
 import Home from './components/Home/index'
-import FlashSales from './components/FlashSales';
-import Best from './components/Best';
-import Explore from './components/Explore';
-// import ItemViewer from './components/ItemViewer';
-import NewArrival from './components/NewArrival';
-import Features from './components/Features';
-import Router from './Route';
-import Category from "./components/Category";
-
-
+import ProductDetail from './Pages/ProductDetail'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <div className='App'>
-      <Home />
-      <FlashSales />
-      <Category />
-      <Best />
-      <Explore />
-      <NewArrival />
-      <Features />
-    </div>
+    <>
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                    <Route path="/Product" element={<ProductDetail />} />
+                </Route>
+                <Route path="*" element={<>4040page not found</>} />
+            </Routes>
+        </BrowserRouter>
+    </>
   );
 }
 export default App;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Header from '../Header/index' 
 import Hero from '../Hero/index'
 import Footer from '../Footer/index'
 import ColorsButton from '../ColorButtons/index'
+import FlashSales from '../FlashSales';
+import Best from '../Best';
+import Explore from '../Explore';
+// import ItemViewer from './components/ItemViewer';
+import NewArrival from '../NewArrival';
+import Features from '../Features';
+import Container from '@mui/material/Container';
+import Category from "../Category";
+
+import Speaker from "../Speaker";
 import './style.css';
 
 function index() {
   return (
-
     <>
-     <CssBaseline/>
-     <Container maxWidth="lg" >
+      <CssBaseline />
+      <Container maxWidth="lg" >
         <Header />
-      </Container>
-
-      <Container maxWidth="lg" >
         <Hero />
-      </Container>
-
-      <Container maxWidth="lg" >
-
+        <Container sx={{ maxWidth:'95%'  }} maxWidth={false} >
+          <FlashSales />
+          <Category />
+          <Best />
+          <Speaker />
+          <Explore />
+          <NewArrival />
+          <Features />
+        </Container>
         <Footer />
       </Container>
-   
-  {/* <ColorsButton /> */}
     </>
-
   )
 }
-
 export default index
